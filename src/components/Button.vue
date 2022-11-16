f5e3ec2556214ebb1ca0d85eceb87584864ce189
riyan-eng <fbriyant11@gmail.com>
2022-11-16 19:04:29 +0700

    mulai

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+    <button class="btn">{{text}}</button>
+</template>
+
+<script>
+export default{
+    name: 'Button',
+    props:{
+        text: String,
+        color: String
+    }
+}
+</script>
+
