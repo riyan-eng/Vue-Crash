1cce225b916b9df266ccae93170627c205726b8c
riyan-eng <fbriyant11@gmail.com>
2022-11-17 09:43:37 +0700

    crud api json-server

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker" @toggle-add-task="toggleAddTask" 
-      :showAddTask="showAddTask"
-    />
+    <Header title="Task Tracker" @toggle-add-task="toggleAddTask" :showAddTask="showAddTask" />
     <div v-show="showAddTask">
       <AddTask @add-task="addTask" />
 
@@ -35,43 +33,62 @@ export default {
     toggleAddTask() {
       this.showAddTask = !this.showAddTask
     },
-    addTask(task) {
-      this.tasks = [...this.tasks, task]
+    async addTask(task) {
+      const res = await fetch('http://localhost:5000/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(task)
+      })
+
+      const data = await res.json()
+
+      this.tasks = [...this.tasks, data]
     },
 
-    deleteTask(id) {
+    async deleteTask(id) {
       if (confirm('are you sure?')) {
-        this.tasks = this.tasks.filter((task) => task.id !== id)
-        console.log("task", id)
+        const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+          method: 'DELETE'
+        })
+
+        res.status === 200 ? this.tasks = this.tasks.filter((task) => task.id !== id) : alert('Error deleting data')
       }
     },
-    toggleReminder(id) {
-      console.log('toggle', id)
-      this.tasks = this.tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task)
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id)
+      const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+      const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+        method:'PUT',
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(updTask)
+      })
+
+      const data = await res.json()
+
+      // console.log(data)
+      this.tasks = this.tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task)
+    },
+
+    async fetchTasks() {
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+      return data
+    },
+
+    async fetchTask(id) {
+      const res = await fetch(`http://localhost:5000/tasks/${id}`)
+      const data = await res.json()
+      return data
     }
   },
 
-  created() {
-    this.tasks = [
-      {
-        id: 1,
-        text: 'Doctor appointment',
-        day: 'March 1st at 2:30pm',
-        reminder: true
-      },
-      {
-        id: 2,
-        text: 'Meeting at school',
-        day: 'March 3rd at 1:30pm',
-        reminder: true
-      },
-      {
-        id: 3,
-        text: 'Food shopping',
-        day: 'March 3rd at 11:00am',
-        reminder: false
-      }
-    ]
+  async created() {
+    this.tasks = await this.fetchTasks()
   }
 }
 </script>
